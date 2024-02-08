ee3156d5b70dc5193a3949c6db349467b470d781
unknown <you@example.com>
2024-02-08 17:28:18 +0530

    prince4

---
+++ project2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    long long num;
+    int count = 0;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    do
+    {
+        count++;
+        num /= 10;
+    } while(num != 0);
+
+    printf("Total digits: %d", count);
+}
